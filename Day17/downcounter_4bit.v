c74ae062be6c792660dfef4911048136bbd5f45e
SomanSaroj <somansaroj@gmail.com>
2025-05-14 21:16:23 +0530

    Day 17 DownCounter Verilog code updated

---
+++ Day17/downcounter_4bit.v
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 14.05.2025 14:26:48
+// Design Name: 
+// Module Name: downcounter_4bit
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module downcounter_4bit(
+    output [3:0] y,
+    input clk,
+    input rst
+    );
+    reg [3:0]count;
+        always @(posedge clk or posedge rst) begin
+            if(rst) begin count<=4'b1111; end
+            else begin
+                count<=count+1'b1;
+                end
+                
+         end
+         assign y=count;
+endmodule
