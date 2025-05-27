b78a6424e048fe48a5a25843c543298d5351514b
SomanSaroj <somansaroj@gmail.com>
2025-05-27 22:42:08 +0530

    Day 30 Sequence Detector Verilog Code Updated

---
+++ Day30/sequence_detector.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 27.05.2025 22:28:48
+// Design Name: 
+// Module Name: sequence_detector
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module sequence_detector(
+    output detected,
+    input clk,
+    input reset,
+    input data_in
+    );
+endmodule
